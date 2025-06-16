be4064f040f226413851d1d41a0484f9ed3af6cf
ryzhenkov.d <ryzhenkov.d@promail366.com>
2025-06-16 16:11:24 +0300

    1.ts

--- 1.ts
+++ 1.ts
@@ -1,1 +1,9 @@
+const sums = [1, 5, 100];
 
+const state: {sumAfterConvert: number[]} = {
+    sumAfterConvert: []
+};
+
+const getValueByAmount = (amount: number) => {
+    return state.sumAfterConvert[amount];
+};
